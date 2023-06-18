788c70732135a685310304deb619bbae5a17adc6
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-06-18 11:54:16 +0700

    add saving state in options page

--- src/options/options.js
+++ src/options/options.js
@@ -1,20 +1,42 @@
 const optionsForm = document.querySelector("#options-form");
 const serverInput = document.querySelector("#server-input");
 const tokenInput = document.querySelector("#token-input");
+const stateLoading = document.querySelector("#state-loading");
+const stateSuccess = document.querySelector("#state-success");
+const stateError = document.querySelector("#state-error");
 
 const value = {
   server: "",
   token: ""
 };
 
+function showState(state) {
+  if (state === "loading") {
+    stateLoading.className = "";
+    stateSuccess.className = "hidden";
+    stateError.className = "hidden";
+  } else if (state === "success") {
+    stateLoading.className = "hidden";
+    stateSuccess.className = "";
+    stateError.className = "hidden";
+  } else if (state === "error") {
+    stateLoading.className = "hidden";
+    stateSuccess.className = "hidden";
+    stateError.className = "";
+  }
+}
+
 async function saveData(server, token) {
+  showState("loading");
   try {
     await browser.storage.local.set({
       server,
       token
     });
+    showState("success");
   } catch (error) {
     console.log("Error saving server and token value", error);
+    showState("error");
   }
 }
 
